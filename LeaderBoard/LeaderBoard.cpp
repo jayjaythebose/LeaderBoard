e41e20b222dc5df25cefeb6a50eb4117b3d97278
jayjaythebose <52707025+jayjaythebose@users.noreply.github.com>
2024-01-30 12:56:43 +0000

    add more stuff

--- LeaderBoard/LeaderBoard.cpp
+++ LeaderBoard/LeaderBoard.cpp
@@ -1,46 +1,75 @@
-// LeaderBoard.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
-#include "LeaderBoard.h"
+
 using namespace std;
-int main()
-{
-	string nameofteam;
-	bool end = false;
-	 
-	 while (end)
-	 {
-		 cout << "Enter your Team: ";
-		 cin >> nameofteam;
-		 nameofteam = Teams[12];
-		
 
+const int NUM_TEAMS = 12;
 
-	 };
-}
-class Team 
+class Team
 {
- public: 
-	 string teamName = nameofteam;
-	 int MP;
-	 int W;
-	 int D;
-	 int L;
-	 int GF;
-	 int GA;
-	 int GD;
-	 int P;
+public:
+    string teamName;
+    int MP;
+    int W;
+    int D;
+    int L;
+    int GF;
+    int GA;
+    int GD;
+    int P;
+
+    // Constructor to initialize member variables
+    Team() : MP(0), W(0), D(0), L(0), GF(0), GA(0), GD(0), P(0) {}
 };
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main()
+{
+    Team Teams[NUM_TEAMS];
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    // Initialize teams with default values
+    for (int i = 0; i < NUM_TEAMS; ++i)
+    {
+        Teams[i].teamName = "Team" + to_string(i + 1);
+        Teams[i].MP = Teams[i].W = Teams[i].D = Teams[i].L = 0;
+        Teams[i].GF = Teams[i].GA = Teams[i].GD = Teams[i].P = 0;
+    }
+
+    string nameofteam;
+    bool end = false;
+
+    while (!end)
+    {
+        cout << "Enter your Team (or type 'end' to finish): ";
+        cin >> nameofteam;
+
+        if (nameofteam == "end")
+        {
+            end = true;
+        }
+        else
+        {
+            // Find the team with the entered name and update its statistics
+            for (int i = 0; i < NUM_TEAMS; ++i)
+            {
+                if (Teams[i].teamName == nameofteam)
+                {
+                    // Add logic to update other statistics as needed
+                    // ...
+
+                    // Output the updated leaderboard
+                    cout << "Leaderboard:\n";
+                    for (int j = 0; j < NUM_TEAMS; ++j)
+                    {
+                        cout << Teams[j].teamName << "\tMP: " << Teams[j].MP << "\tW: " << Teams[j].W
+                            << "\tD: " << Teams[j].D << "\tL: " << Teams[j].L << "\tGF: " << Teams[j].GF
+                            << "\tGA: " << Teams[j].GA << "\tGD: " << Teams[j].GD << "\tP: " << Teams[j].P << endl;
+                    }
+
+                    break;  // Exit the loop once the team is found and updated
+                }
+            }
+        }
+    }
+
+    return 0;
+}
--- LeaderBoard/LeaderBoard.h
+++
@@ -1,4 +0,0 @@
-#include <string>
-using namespace std;
-string nameofteam;
-string Teams[12];
